d3992559b89005807abaf1f84e4d12e4f7e4103f
IndiraRamalingam <indiraramalingam93@gmail.com>
2023-06-13 19:45:43 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+// function App() {
+//   console.log("Hello from component1")
+//   return (
+//     <div><p>Hello World !!!</p></div>
+//   );
+// }
+// 
+// export default App;
+// 
+// 
+// import React from 'react'
+// 
+// function Hello()
+// {
+//     return(
+//         <div>
+//             <p>Hello World</p>
+//         </div>
+//     )
+// }
+// 
+// function App() {
+//   return (
+//     <div class="container">
+//         <div class="row">
+//             <div class="col-4">
+//                 Hi
+//             </div>
+//             <div class="col-4">
+//                 Hello
+//             </div>
+//             <div class="col-4">
+//                 World
+//             </div>
+//         </div>
+//         <h1>Header</h1>
+//         <Hello />
+//         <Hello />
+//     </div>
+//   )
+// }
+// 
+// export default App
+
+//Day 3
+
+import React from 'react'
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+//data
+const notes=[
+    {
+        id:1,
+        Content:'Java',
+        important:true
+    },
+    {
+        id:2,
+        Content:'HTML',
+        important:false
+    },
+    {
+        id:3,
+        Content:'CSS',
+        important:true
+    },{
+        id:4,
+        Content:'Bootstrap',
+        important:false
+    },{
+        id:5,
+        Content:'React',
+        important:true
+    }
+]
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
