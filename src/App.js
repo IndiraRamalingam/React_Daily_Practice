aef48a7d5a5c1ffa2dba8becac5f018efa81b5c1
IndiraRamalingam <indiraramalingam93@gmail.com>
2023-06-14 11:23:41 +0530

    Passing props in Functional and Class component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+//Functional Component
 // function App() {
 //   console.log("Hello from component1")
 //   return (
@@ -6,26 +8,62 @@
 // }
 // 
 // export default App;
-// 
-// 
+
+// Function component and passing props  
 // import React from 'react'
 // 
+// function Hello(props)
+// {
+//     return(
+//         <div>
+//             Hello, {props.name}!!
+//         </div>
+//     )
+// }
 // function App() {
 //   return (
-//     <div>App</div>
+//     <div>
+//         <Hello name={'Indira'}/>
+//     </div>
 //   )
 // }
 // 
 // export default App
 
 //Class Component
-import { Component } from "react";
-class App extends Component{
-    render()
-    {
-        return(
-            <div>Hello</div>
-        )
-    }
-}
-export default App;
+// import { Component } from "react";
+// class App extends Component{
+//     render()
+//     {
+//         return(
+//             <div>Hello</div>
+//         )
+//     }
+// }
+// export default App;
+
+//Passing props in class component
+
+// import { Component } from "react";
+// 
+// class Hello extends Component{
+//     render()
+//     {
+//         console.log(this.props)
+//         return(
+//             <div>Hello World!!! {this.props.name}</div>
+//         )
+//     }
+// }
+// 
+// class App extends Component{
+//     render()
+//     {
+//         return(
+//             <div>
+//                 <Hello name={'krish'} />
+//             </div>
+//         )
+//     }
+// }
+// export default App;
--- src/index.js
+++ src/index.js
@@ -12,30 +12,31 @@ import App from './App';
 
 
 //data
-const notes=[
-    {
-        id:1,
-        Content:'Java',
-        important:true
-    },
-    {
-        id:2,
-        Content:'HTML',
-        important:false
-    },
-    {
-        id:3,
-        Content:'CSS',
-        important:true
-    },{
-        id:4,
-        Content:'Bootstrap',
-        important:false
-    },{
-        id:5,
-        Content:'React',
-        important:true
-    }
-]
+// const notes=[
+//     {
+//         id:1,
+//         Content:'Java',
+//         important:true
+//     },
+//     {
+//         id:2,
+//         Content:'HTML',
+//         important:false
+//     },
+//     {
+//         id:3,
+//         Content:'CSS',
+//         important:true
+//     },{
+//         id:4,
+//         Content:'Bootstrap',
+//         important:false
+//     },{
+//         id:5,
+//         Content:'React',
+//         important:true
+//     }
+// ]
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes}/>)
+//ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes}/>)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
