5770426d2b60d05f1456c23ba8d2ec30605bd5a0
IndiraRamalingam <indiraramalingam93@gmail.com>
2023-06-16 21:23:59 +0530

    useParams

--- src/App.js
+++ src/App.js
@@ -76,8 +76,69 @@
 
 //With React Router --> Install npm install react-router-dom
 
+// import React from 'react'
+// import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// 
+// function Home()
+// {
+//   return(
+//     <div>
+//       <h2>Home Component</h2>
+//     </div>
+//   )
+// }
+// 
+// function Notes()
+// {
+//   return(
+//     <div>
+//       <h2>Notes Component</h2>
+//     </div>
+//   )
+// }
+// 
+// function Users()
+// {
+//   return(
+//     <div>
+//       <h2>Users Component</h2>
+//     </div>
+//   )
+// }
+// 
+// function App() {
+// 
+//   const padding={
+//     padding:10
+//   };
+//   
+//   return (
+//     
+//       <Router>
+//         <div>
+//           <Link to='/' style={padding}>Home</Link>
+//           <Link to='/notes' style={padding}>Notes</Link>
+//           <Link to='/users' style={padding}>Users</Link>
+//         </div>
+// 
+//         <Routes>
+//           <Route path='/' element={<Home />} />
+//           <Route path='/notes' element={<Notes />} />
+//           <Route path='/users' element={<Users />} />
+//        </Routes>
+//         </Router>
+//     
+//     
+//   )
+// }
+// 
+// export default App
+
+
+//With Params
+
 import React from 'react'
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Link, Route, BrowserRouter as Router, Routes, useParams } from 'react-router-dom';
 
 function Home()
 {
@@ -87,12 +148,30 @@ function Home()
     </div>
   )
 }
+function Note ({notes})
+{
+const id=useParams().id;
+const note=notes.find(n =>n.id ===Number(id));
 
-function Notes()
+return(
+  <div>
+    <h2>{note.content}</h2>
+  </div>
+)
+}
+
+function Notes({notes})
 {
   return(
     <div>
       <h2>Notes Component</h2>
+      <ul>
+        {
+          notes.map(note =>
+            <li><Link to={`/notes/${note.id}`}>{note.content}</Link></li>
+            )
+        }
+      </ul>
     </div>
   )
 }
@@ -108,6 +187,21 @@ function Users()
 
 function App() {
 
+  const notes=[
+    {
+      id:1,
+      content:'Java1'
+    },
+    {
+      id:2,
+      content:'Java2'
+    },
+    {
+      id:3,
+      content:'Java3'
+    }
+  ]
+
   const padding={
     padding:10
   };
@@ -122,8 +216,9 @@ function App() {
         </div>
 
         <Routes>
+          <Route path='/notes/:id' element={<Note notes={notes}/>}/>
           <Route path='/' element={<Home />} />
-          <Route path='/notes' element={<Notes />} />
+          <Route path='/notes' element={<Notes notes={notes}/>} />
           <Route path='/users' element={<Users />} />
        </Routes>
         </Router>
